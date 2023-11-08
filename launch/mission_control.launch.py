1f35284188106fdda480644f0e73b154f19f6682
VineetTambe <tambevineet@gmail.com>
2023-11-08 18:27:28 -0500

    updated launch file

--- launch/mission_control.launch.py
+++ launch/mission_control.launch.py
@@ -1,36 +1,55 @@
-import launch_ros.actions
 import os
 from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration
 from ament_index_python.packages import get_package_share_directory
+from launch.actions import GroupAction
+from launch.conditions import IfCondition
+from launch.substitutions import PythonExpression
+from launch_ros.actions import Node
+
 
 def generate_launch_description():
     ld = LaunchDescription()
 
-    #### Define the params ####
-    #! Defined robot0 and robot1 only for sim
+    single_launch = "False"
+
     robot1_namespace = LaunchConfiguration("robot1_namespace", default="robot1")
     robot2_namespace = LaunchConfiguration("robot2_namespace", default="robot2")
 
     params = os.path.join(
-        get_package_share_directory('robot_mission_control'),
-        'config',
-        'params.yaml'
-        )
-
-    # Launch the State Machine node with arguments
-    MissonControl = launch_ros.actions.Node(
-        package="robot_mission_control",
-        executable="robot_mission_control_node",  # NOTE: Executable name defined in setup.py
-        name="robot_mission_control_node",
-        output="screen",
-        parameters=[
-            {"robot1_namespace_param": robot1_namespace},
-            {"robot2_namespace_param": robot2_namespace},
-            {'mission_params': params}
-        ],
+        get_package_share_directory("robot_mission_control"), "config", "params.yaml"
     )
 
-    ld.add_action(MissonControl)
+    # Launch the State Machine node with arguments
+    mission_control_node = GroupAction(
+        actions=[
+            Node(
+                condition=IfCondition(PythonExpression(["not ", single_launch])),
+                package="robot_mission_control",
+                executable="robot_mission_control_node",
+                name="robot_mission_control_node",
+                output="screen",
+                parameters=[
+                    {"robot1_namespace_param": robot1_namespace},
+                    {"robot2_namespace_param": robot2_namespace},
+                    {"mission_params": params},
+                ],
+            ),
+            Node(
+                condition=IfCondition(PythonExpression([single_launch])),
+                package="robot_mission_control",
+                executable="robot_mission_control_node_single",
+                name="robot_mission_control_node_single",
+                output="screen",
+                parameters=[
+                    # {"robot1_namespace_param": robot1_namespace},
+                    {"robot2_namespace_param": robot2_namespace},
+                    {"mission_params": params},
+                ],
+            ),
+        ]
+    )
+
+    ld.add_action(mission_control_node)
 
     return ld
