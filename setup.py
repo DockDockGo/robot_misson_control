aeb133f5390607a29caaf454940b00213284392f
sushanth <sushanth.jayanth@gmail.com>
2023-11-13 08:59:21 -0500

    updated setup.py to reflect launch file changes of previous commit

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
         ("share/" + package_name, ["launch/mission_control.launch.py"]),
-        ("share/" + package_name, ["launch/mission_control_single.launch.py"]),
+        # ("share/" + package_name, ["launch/mission_control_single.launch.py"]),
         (os.path.join("share", package_name, "config"), glob("config/*.yaml")),
     ],
     install_requires=["setuptools"],
